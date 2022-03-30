0b7df658e330082f16b0caa8f83e28d0c6d4a0b5
Sohaib Kaidali <kaidalisohaib@gmail.com>
2022-03-30 10:46:09 -0400

    day5 solved adventofcode

---
+++ AdventOfCode/2015/day4/src/main.rs
@@ -0,0 +1,45 @@
+use md5;
+use std::{fs, process};
+fn main() {
+    let file_content: String = read_input_file();
+    println!("Input:\n{}", file_content);
+
+    let part_one_answer = solve_part_one(&file_content);
+    println!("Part one answer: {:?}", part_one_answer);
+    let part_two_answer = solve_part_two(&file_content);
+    println!("Part two answer: {:?}", part_two_answer);
+}
+
+fn solve_part_one(input: &str) -> u32 {
+    let mut number: u32 = 1;
+    loop {
+        let key: String = format!("{}{}", input, number);
+        let hash: String = format!("{:?}", md5::compute(&key));
+        if (hash.starts_with("00000")) {
+            return number;
+        }
+        number += 1;
+    }
+}
+
+fn solve_part_two(input: &str) -> u32 {
+    let mut number: u32 = 1;
+    loop {
+        let key: String = format!("{}{}", input, number);
+        let hash: String = format!("{:?}", md5::compute(&key));
+        if (hash.starts_with("000000")) {
+            return number;
+        }
+        number += 1;
+    }
+}
+
+fn read_input_file() -> String {
+    match fs::read_to_string("input.txt") {
+        Ok(content) => return content,
+        Err(err) => {
+            eprintln!("Error while opening the input file: {:?}", err);
+            process::exit(1);
+        }
+    };
+}
